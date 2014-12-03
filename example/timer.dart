b4a7a2e940e590e464fef52dd230e50f7ccdc9cb
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-03 14:04:46 -0500

    bug fix in timer

--- example/timer.dart
+++ example/timer.dart
@@ -3,10 +3,12 @@ import "package:console/console.dart";
 
 void main() {
   var timer = new TimeDisplay();
-  
+
+  Terminal.write("Waiting 10 Seconds ");
   timer.start();
   
   new Future.delayed(new Duration(seconds: 10)).then((_) {
     timer.stop();
+    print("");
   });
 }
