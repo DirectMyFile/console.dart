a0de07224fd1298a5a16ced1d98924795959f537
Michael Bullington <mikebullingtn@gmail.com>
2015-02-01 19:26:07 -0500

    Add clipboard support for OSX.

--- lib/src/clipboard.dart
+++ lib/src/clipboard.dart
@@ -1,48 +1,57 @@
 part of console;
 
-/// Very limited clipboard support.
-/// 
-/// **NOTICE**: This is very unstable so far. I don't recommend you use it yet.
-class Clipboard {
-  static bool _isSupported;
-  
-  static bool get isSupported {
-    if (_isSupported == null) {
-      _determineSupport();
-    }
-    return _isSupported;
-  }
-  
-  static void _determineSupport() {
-    bool supported = false;
-    if (Platform.isLinux) {
-      if (new File("/usr/bin/xclip").existsSync()) {
-        supported = true;
-      }
-    }
-    _isSupported = supported;
-  }
-  
-  static String getClipboard() {
-    if (!isSupported) {
-      throw new UnsupportedError("Clipboard Features are not supported in your environment.");
-    }
+Clipboard getClipboard() {
+  if (Platform.isMacOS)
+    return new OSXClipboard();
+  if (Platform.isLinux && new File("/usr/bin/xclip").existsSync())
+    return new XClipboard();
+  return null;
+}
 
-    var result = Process.runSync("/usr/bin/xclip", ["-selection", "clipboard", "-o"]);
+class Clipboard {
+  String getContent();
+
+  String setContent(String content);
+}
+
+class OSXClipboard implements Clipboard {
+
+  @override
+  String getContent() {
+    var result = Process.runSync("/usr/bin/pbpaste");
     if (result.exitCode != 0) {
-      throw new Exception("Failed to get clipboard. Exit Code was not 0");
+      throw new Exception("Failed to get clipboard content.");
     }
     return result.stdout.toString();
   }
-  
-  static void setClipboard(String content) {
-    if (!isSupported) {
-      throw new UnsupportedError("Clipboard Features are not supported in your environment.");
+
+  @override
+  void setContent(String content) {
+    Process.start("/usr/bin/pbpaste").then((process) {
+      process.stdin.write(content);
+      process.stdin.close();
+    });
+  }
+
+}
+
+class XClipboard implements Clipboard {
+
+  @override
+  String getContent() {
+    var result = Process.runSync("/usr/bin/xclip", ["-selection", "clipboard", "-o"]);
+    if (result.exitCode != 0) {
+      throw new Exception("Failed to get clipboard content.");
     }
-    
+    return result.stdout.toString();
+  }
+
+  @override
+  void setContent(String content) {
     Process.start("/usr/bin/xclip", ["-selection", "clipboard"]).then((process) {
       process.stdin.write(content);
       process.stdin.close();
     });
   }
+
 }
